99d00649f293a1d597fcaf5f13d43b4911f346a0
tyru <tyru.exe@gmail.com>
2010-04-30 00:57:12 +0900

    Add more examples.

--- 2010-04-29.pl
+++ 2010-04-29.pl
@@ -8,12 +8,37 @@ use Acme::FizzBuzz::Descriptive;
 
 fizzbuzz {
     from 1;
-    to 100;
+    to 15;
+    rule { print "Fizz" } where { $_ % 3 == 0 };
+    rule { print "Buzz" } where { $_ % 5 == 0 };
+    fallback { print $_ };
+    each_loop_end { print "\n" };
+};
+
+print "\n\n";
+
+fizzbuzz {
+    from 1;
+    to 15;
     rule { print "$_:Fizz " } where { $_ % 3 == 0 };
     rule { print "$_:Buzz " } where { $_ % 5 == 0 };
     fallback { print $_ };
     each_loop_end { print "\n" };
 };
+
+print "\n\n";
+
+fizzbuzz {
+    from 1;
+    to 15;
+    rule { print "Fizz" } where { $_ % 3 == 0 };
+    rule { print "Buzz" } where { $_ % 5 == 0 };
+    fallback { print $_ };
+};
+
+print "\n\n";
+
+
 
 no Acme::FizzBuzz::Descriptive;
 # fizzbuzz;    # error
